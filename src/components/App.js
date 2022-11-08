ff28943d57a6147a98a2b3cebf5013744e4d7e5e
Zaid Almuflahi <zaid.almuflahi1217@gmail.com>
2022-11-08 14:26:03 -0500

    commit tree fcf077a926470b839560baf0eafd8e01cc66392f

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,67 @@
 import React from "react";
 import blogData from "../data/blog";
 
-console.log(blogData);
+
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name={blogData.name} />
+      <About image={blogData.image} about={blogData.about} />
+      <ArticleList posts={blogData.posts}/>
     </div>
-  );
+  )}
+
+function Header(props){
+return (
+  <header><h1 className="tle">{props.name}</h1></header>
+    
+)}
+
+function About(props){
+return(
+  <aside>
+      <img id='pic'src={props.image} placeholder='https://via.placeholder.com/215' alt='blog logo'></img>
+      <p id='about'>{props.about}</p>
+  </aside>
+)}
+
+function Article(props){
+    let string = ''
+    if(props.minutes > 30){
+      let m = props.minutes;
+      let num = Math.round(m/10)
+      for(let i= 0; i<num; i++){
+        string += "🍱"
+      }
+      string += `${m} min read`
+    }else{
+      let m = props.minutes;
+      let num = Math.round(m/5)
+      for(let i= 0; i<num; i++){
+        string += "☕️"
+      }
+      string += `${m} min read`
+    }
+return (
+  <article>
+    <h3>{props.title}</h3>
+    <small>{props.date}</small>
+    <p>{props.preview}</p>
+    <p>{string}</p>
+  </article>
+)
 }
+function ArticleList(props){
+    return (
+      <main>
+      {props.posts.map((post) =>{
+        return (
+          <Article key={post.id} title={post.title} date={post.date} preview={post.preview} minutes={post.minutes}/>
+        )})}
+      </main>
+    )}
+
+
 
 export default App;
